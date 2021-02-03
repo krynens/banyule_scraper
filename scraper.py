2d833847e99f8f19f6d2804a678c8833128f7337
krynens <75901402+krynens@users.noreply.github.com>
2021-02-03 19:10:46 +1100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,27 +1,64 @@
-import scraperwiki
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import time
-import requests
-import sys
+import csv
+import datetime 
 
-expected_string = "Hassle-free web scraping."
+# Define and open CSV File
+csv_file = open('portphillip_ad.csv', 'w+')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Number', 'Description', 'Lodged', 'Address', 'Documents', 'Date Scraped'])
 
-# Check that proxy gives a working certificate for SSL connections
-# If the certificate isn't valid it should throw an exception
-html = scraperwiki.scrape("https://morph.io")
+driver.get('https://eservices.portphillip.vic.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryLists.aspx')
+print('Starting Port Phillip Ad scraper...')
+print()
 
-if not expected_string in html:
-    raise Exception("Not expected result")
+# Define and Click Buttons
+advertising = driver.find_element_by_id('ctl00_MainBodyContent_mDataList_ctl03_mDataGrid_ctl02_ctl00')
+advertising.click()
 
-# Use requests library to do the same because it gets its CA certs a different way. Oh joy.
-r = requests.get('https://morph.io')
-if not expected_string in r.text:
-    raise Exception("Not expected result")
+next = driver.find_element_by_id('ctl00_MainBodyContent_mContinueButton')
+next.click()
 
-# Write out to the sqlite database using scraperwiki library
-scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
+# Define Application Variables
+number = driver.find_elements_by_xpath('//*[@id="ctl00_MainBodyContent_group_11"]/table/tbody/tr/td/table/tbody/tr/td[1]/a')
+description = driver.find_elements_by_xpath('//*[@id="ctl00_MainBodyContent_group_11"]/table/tbody/tr/td/table/tbody/tr/td[3]/div')
+address = driver.find_elements_by_xpath('//*[@id="ctl00_MainBodyContent_group_11"]/table/tbody/tr/td/table/tbody/tr/td[2]/div')
+documents = driver.find_elements_by_xpath('//*[@id="ctl00_MainBodyContent_group_11"]/table/tbody/tr/td/table/tbody/tr/td[1]/a')
+lodged = driver.find_elements_by_xpath('//*[@id="ctl00_MainBodyContent_group_11"]/table/tbody/tr/td/table/tbody/tr/td[4]/span')
 
-sys.stderr.write("First a little test message to stderr\n")
+# Write to CSV File
+page_items = len(number)
+current_page = driver.find_element_by_id('ctl00_MainBodyContent_mPagingControl_pageNumberLabel')
+print('Getting ' + current_page.text)
+with open('portphillip_ad.csv', 'w+'):
+    for i in range(page_items):
+        csv_writer.writerow([number[i].text, description[i].text, lodged[i].text, address[i].text, documents[i].get_attribute('href'), today])
 
-for i in range(1, 6):
-    print "%i..." % i
-    time.sleep(1)
+# Defining Append File Function
+def append_file():
+    next_page = driver.find_element_by_id('ctl00_MainBodyContent_mPagingControl_nextPageHyperLink')
+    next_page.click()
+    current_page = driver.find_element_by_id('ctl00_MainBodyContent_mPagingControl_pageNumberLabel')
+    print('Getting ' + current_page.text)
+    number = driver.find_elements_by_xpath('//*[@id="ctl00_MainBodyContent_group_11"]/table/tbody/tr/td/table/tbody/tr/td[1]/a')
+    description = driver.find_elements_by_xpath('//*[@id="ctl00_MainBodyContent_group_11"]/table/tbody/tr/td/table/tbody/tr/td[3]/div')
+    address = driver.find_elements_by_xpath('//*[@id="ctl00_MainBodyContent_group_11"]/table/tbody/tr/td/table/tbody/tr/td[2]/div')
+    documents = driver.find_elements_by_xpath('//*[@id="ctl00_MainBodyContent_group_11"]/table/tbody/tr/td/table/tbody/tr/td[1]/a')
+    lodged = driver.find_elements_by_xpath('//*[@id="ctl00_MainBodyContent_group_11"]/table/tbody/tr/td/table/tbody/tr/td[4]/span')
+    with open('portphillip_ad.csv', 'a+'):
+        for i in range(page_items):
+            csv_writer.writerow([number[i].text, description[i].text, lodged[i].text, address[i].text, documents[i].get_attribute('href'), today])
+
+# Append File Loop
+for i in range(10000):
+    try:
+        append_file()
+    except:
+        print()
+        print('Scraper finished. No more pages to get.')
+        break
+
+# Close and Save
+csv_file.close()
+driver.close()
