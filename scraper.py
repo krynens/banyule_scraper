7fd974c3b16093e7f8741b2721a47dd883035d71
krynens <75901402+krynens@users.noreply.github.com>
2021-03-06 08:34:01 +1100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -8,29 +8,23 @@ from bs4 import BeautifulSoup
 
 today = datetime.today()
 
-for i in range(1, 10):
-    try:
-        print(f'Getting page {i}')
-        url = 'https://www.banyule.vic.gov.au/Planning-building/Review-local-planning-applications/Planning-applications-on-public-notice?dlv_OC%20CL%20Public%20Works%20and%20Projects=(pageindex=' + str(i) + ')'
-        r = requests.get(url)
-        soup = BeautifulSoup(r.content, 'lxml')
-        rows = soup.find_all('div', class_='list-item-container item-no-date')
 
-        for row in rows:
-            record = {}
-            record['address'] = row.find(
-                'h2', class_='list-item-title').text.split(' - ')[0].strip()
-            record['date_scraped'] = today.strftime("%d-%m-%Y")
-            record['council_reference'] = row.find(
-                'h2', class_='list-item-title').text.split(' - ')[1].strip()
-            record['info_url'] = str(row.find('a')).split('"')[1]
-            on_notice_to_raw = row.find('p').text.split(': ')[1]
-            record['on_notice_to'] = datetime.strptime(
-                on_notice_to_raw, '%d %B %Y').strftime('%Y-%m-%d')
+url = 'https://www.banyule.vic.gov.au/Planning-building/Review-local-planning-applications/Planning-applications-on-public-notice?dlv_OC%20CL%20Public%20Works%20and%20Projects=(pageindex=1'
+r = requests.get(url)
+soup = BeautifulSoup(r.content, 'lxml')
+rows = soup.find_all('div', class_='list-item-container item-no-date')
 
-            scraperwiki.sqlite.save(
-                unique_keys=['council_reference'], data=record, table_name="data")
+for row in rows:
+    record = {}
+    record['address'] = row.find(
+        'h2', class_='list-item-title').text.split(' - ')[0].strip()
+    record['date_scraped'] = today.strftime("%d-%m-%Y")
+    record['council_reference'] = row.find(
+        'h2', class_='list-item-title').text.split(' - ')[1].strip()
+    record['info_url'] = str(row.find('a')).split('"')[1]
+    on_notice_to_raw = row.find('p').text.split(': ')[1]
+    record['on_notice_to'] = datetime.strptime(
+        on_notice_to_raw, '%d %B %Y').strftime('%Y-%m-%d')
 
-    except:
-        print('Scraper finished.')
-        break
+    scraperwiki.sqlite.save(
+        unique_keys=['council_reference'], data=record, table_name="data")
