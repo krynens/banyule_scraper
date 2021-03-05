7418cfc6015e8d003ce0af4ccbdf5f67e4ce7011
krynens <75901402+krynens@users.noreply.github.com>
2021-03-05 23:32:39 +1100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,34 +1,37 @@
 import os
 os.environ["SCRAPERWIKI_DATABASE_NAME"] = "sqlite:///data.sqlite"
 
-from bs4 import BeautifulSoup
-from datetime import datetime
-import requests
 import scraperwiki
+import requests
+from datetime import datetime
+from bs4 import BeautifulSoup
 
 today = datetime.today()
 
-for i in range(1, 50):
+urlt = 'https://www.banyule.vic.gov.au/Planning-building/Review-local-planning-applications/Planning-applications-on-public-notice?dlv_OC%20CL%20Public%20Works%20and%20Projects=(pageindex=1)'
+rt = requests.get(urlt)
+soupt = BeautifulSoup(rt.content, 'lxml')
+max_page = int(soupt.find('div', class_='seamless-pagination-info right').text.split('of ')[1])
+
+for i in range(1, max_page + 1):
     try:
         print(f'Getting page {i}')
-        url = 'https://www.melbourne.vic.gov.au/building-and-development/property-information/planning-building-registers/Pages/town-planning-permits-register-search-results.aspx?AdvertisingOnly=on&page=' + str(i)
+        url = f'https://www.banyule.vic.gov.au/Planning-building/Review-local-planning-applications/Planning-applications-on-public-notice?dlv_OC%20CL%20Public%20Works%20and%20Projects=(pageindex={i})'
         r = requests.get(url)
         soup = BeautifulSoup(r.content, 'lxml')
-
-        table = soup.find('tbody')
-        rows = table.find_all('tr', class_='detail')
+        rows = soup.find_all('div', class_='list-item-container item-no-date')
 
         for row in rows:
             record = {}
-            record['address'] = row.find('td', class_='column2').text
-            date_received_raw = row.find('td', class_='column3').text
-            record['date_received'] = datetime.strptime(
-                date_received_raw, "%d/%m/%Y").strftime("%Y-%m-%d")
-            record['date_scraped'] = today.strftime("%Y-%m-%d")
-            record['description'] = row.find('td', class_='column4').text
-            record['council_reference'] = row.find('td', class_='column1').text
-            record['info_url'] = 'https://www.melbourne.vic.gov.au' + \
-                str(row.find('td', class_='column1')).split('"')[5]
+            record['address'] = row.find(
+                'h2', class_='list-item-title').text.split(' - ')[0].strip()
+            record['date_scraped'] = today.strftime("%d-%m-%Y")
+            record['council_reference'] = row.find(
+                'h2', class_='list-item-title').text.split(' - ')[1].strip()
+            record['info_url'] = str(row.find('a')).split('"')[1]
+            on_notice_to_raw = row.find('p').text.split(': ')[1]
+            record['on_notice_to'] = datetime.strptime(
+                on_notice_to_raw, '%d %B %Y').strftime('%Y-%m-%d')
 
             scraperwiki.sqlite.save(
                 unique_keys=['council_reference'], data=record, table_name="data")
