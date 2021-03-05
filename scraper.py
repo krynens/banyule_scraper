715a131f67645eeb0de40addaa6dc5dfb11fd764
krynens <75901402+krynens@users.noreply.github.com>
2021-03-05 16:06:26 +1100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,16 +1,16 @@
 from bs4 import BeautifulSoup
 from datetime import datetime
-import datetime
 import requests
 import scraperwiki
 import os
 os.environ["SCRAPERWIKI_DATABASE_NAME"] = "sqlite:///data.sqlite"
 
 
-today = datetime.date.today()
+today = datetime.today()
 
 for i in range(1, 50):
     try:
+        print(f'Getting page {i}')
         url = 'https://www.melbourne.vic.gov.au/building-and-development/property-information/planning-building-registers/Pages/town-planning-permits-register-search-results.aspx?AdvertisingOnly=on&page=' + str(i)
         r = requests.get(url)
         soup = BeautifulSoup(r.content, 'lxml')
